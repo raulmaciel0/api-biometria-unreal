d6decdbee10d76b14f8c49c44ec658dd14510811
Raul <rgermino@virtuatechengenharia.com.br>
2025-01-23 07:47:39 -0300

    Primeiro commit

---
+++ src/main/java/com/virtuatech/biometria/biometria_api/BiometriaApiApplication.java
@@ -0,0 +1,13 @@
+package com.virtuatech.biometria.biometria_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiometriaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiometriaApiApplication.class, args);
+	}
+
+}
